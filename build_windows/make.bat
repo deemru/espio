6c1cafc5acfc59659c530b43ea4960d5e055c121
Dmitrii Pichulin <deem@deem.ru>
2016-04-05 12:19:24 +0300

    naming: espio_test

--- build_windows/make.bat
+++ build_windows/make.bat
@@ -9,4 +9,4 @@ cl /c /Ox /Os /GL /GF /GS- /W4 /EHsc /I../src ../examples/espio_test.c
 rc -r espio.rc
 
 link /DLL /LTCG espio.obj espio.res /OUT:%OUT%.dll
-link /LTCG espio_test.obj espio.res %OUT%.lib /subsystem:console /OUT:%OUT%.exe
+link /LTCG espio_test.obj espio.res %OUT%.lib /subsystem:console /OUT:%OUT%_test.exe
